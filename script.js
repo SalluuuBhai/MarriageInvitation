9a47fcf5452f24c615bbb62ea3bc6ded2fb808e8
MD Salman Faris <117359170+SalluuuBhai@users.noreply.github.com>
2023-11-16 23:14:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const newYear=new Date(`September 28 ${currentYear} 12:00:00`);
 
 function UpdateTime(){
   const currentDate=new Date();
-  const diff=newYear-currentDate;
+  const diff=newYear+currentDate;
   const d=Math.floor(diff/1000/60/60/24);
   const h=Math.floor((diff/1000/60/60)%24);
   const m=Math.floor((diff/1000/60)%60);
@@ -17,9 +17,9 @@ function UpdateTime(){
   const ms=Math.floor(diff%1000);
 
   days.innerHTML=d<10?""+d:d;
-  hours.innerHTML=h<10?"0"+h:h;
-  minutes.innerHTML=m<10?"0"+m:m;
-  seconds.innerHTML=s<10?"0"+s:s;
+  hours.innerHTML=h<10?""+h:h;
+  minutes.innerHTML=m<10?""+m:m;
+  seconds.innerHTML=s<10?""+s:s;
   // mseconds.innerHTML=ms<10?"0"+ms:ms;
 }
 setInterval(UpdateTime,1000);
